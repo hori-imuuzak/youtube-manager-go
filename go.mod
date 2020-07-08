9fbfda444f227f40a3ddf4f6d9f2d4e680cb715a
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-07-09 01:15:51 +0900

    人気の動画取得API

---
+++ go.mod
@@ -0,0 +1,13 @@
+module youtube-manager-go
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/sirupsen/logrus v1.6.0
+	github.com/valyala/fasthttp v1.14.0
+	golang.org/x/crypto v0.0.0-20200707235045-ab33eee955e0 // indirect
+	google.golang.org/api v0.28.0
+)
---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"youtube-manager-go/routes"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Fatal("Error loading .env")
+	}
+
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+
+	routes.Init(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
